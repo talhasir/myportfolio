384fa9068bc54c0d27677c5cbe170ffde9969b3e
Talha_Office <talha_office@Talhas-MacBook-Pro.local>
2024-11-24 20:00:31 +0500

    commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Hafiz Talha, Let's create something amazing together 
+            Hi! I&apos;m Hafiz Talha, Let&apos;s create something amazing together 
           </p>
 
           <a href="#about">
